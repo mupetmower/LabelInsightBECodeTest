95fba1ec93bbcfc945601ee705da2310a59f89fc
Alex Frye <mupetmower@gmail.com>
2023-03-07 15:10:18 -0500

    Add logic to let this be ran from IDE or terminal

--- src/com/labelinsight/codetest/Main.java
+++ src/com/labelinsight/codetest/Main.java
@@ -4,68 +4,102 @@ import com.labelinsight.codetest.solutions.MissingLetters;
 import com.labelinsight.codetest.solutions.ParticleAnimation;
 
 import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class Main {
-    public static final String errorMsg = "Please include params for method to run (getMissingLetters or animate) and the input string to provide the method, surrounded by quotes\n" +
+    public static final String errorMsgTerminal = "Please include params for method to run (getMissingLetters or animate) and the input string to provide the method, surrounded by quotes\n" +
             "To run getMissingLetters use: getMissingLetters \"input\"\n" +
             "To run animate use: animate param2: speed \"init\"\n";
+
+    public static final String errorMsgIDE = "Please use prompts to enter the method to run and parameters for the method.";
 
     private boolean runAsBenchmark = false;
     private static int numRuns = 500;
 
     public static void main(String... args) {
-
+        // NOTE - this was first written as if it were going to be ran from terminal, but then realized it would possibly be ran
+        // from an IDE. So I added logic so both can be done.
         try {
-            if (args.length < 2) {
-                System.out.println(errorMsg);
-                throw new Exception();
-            }
+            //Check to see if runnning from a built jar file
+            URL res = Main.class.getResource("Main.class");
+            // If not, assume it is running from IDE and use Scanner.in to get input
+            if (!res.toString().contains("jar")) {
+                Scanner in = new Scanner(System.in);
+                System.out.print("Please enter method to run(getMissingLetters or animate): ");
+                String methodToRun = in.nextLine();
+                int speed;
+                String input = "";
 
-            String methodToRun = args[0];
-            int speed;
-            String input = "";
+                if (methodToRun.equals("getMissingLetters")) {
+                    System.out.print("Please enter the input for getMissingLetters (do not use quotes): ");
+                    input = in.nextLine();
 
-            if (methodToRun.equals("getMissingLetters")) {
-                input = args[1];
+                    getMissingLetters(input);
+                } else if (methodToRun.equals("animate")) {
+                    System.out.print("Please enter the speed value for animate as an integer: ");
 
-                getMissingLetters(input);
-            }
-            else if (methodToRun.equals("animate")) {
-                speed = Integer.parseInt(args[1]);
-                input = args[2];
+                    String spdStr = in.nextLine();
+                    speed = Integer.parseInt(spdStr);
 
-                ParticleAnimation pa = new ParticleAnimation();
-                String output = pa.animate(speed, input);
+                    System.out.print("Please enter the init value for animate (do not use quotes, only use R, L, or . in init with no spaces): ");
+                    input = in.nextLine();
 
-                System.out.println(output);
-            }
-            else {
-                System.out.println(errorMsg);
-            }
+                    ParticleAnimation pa = new ParticleAnimation();
+                    String output = pa.animate(speed, input);
 
-
-
-            // Benchmarking - not enough time to implement into running with console
-
-            /*List<String> argsList = Arrays.asList(args);
-            if (argsList.contains("-runAsBenchmark")) {
-                runBenchmark = true;
-            }
-
-            if (runBenchmark) {
-                try {
-                    getMissingLettersBenchmark(input);
-                } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException | InstantiationException e) {
-                    e.printStackTrace();
+                    System.out.println(output);
+                } else {
+                    System.out.println(errorMsgIDE);
                 }
-            } else {
-                MissingLetters missingLetters = new MissingLetters();
-                missingLetters.getMissingLetters(input);
-            }*/
+            }
+            // If so, run with input from args[] from terminal
+            else {
+                if (args.length < 2) {
+                    System.out.println(errorMsgTerminal);
+                    throw new Exception();
+                }
 
-        }
-        catch (Exception e) {
-            System.out.println(errorMsg);
+                String methodToRun = args[0];
+                int speed;
+                String input = "";
+
+                if (methodToRun.equals("getMissingLetters")) {
+                    input = args[1];
+
+                    getMissingLetters(input);
+                } else if (methodToRun.equals("animate")) {
+                    speed = Integer.parseInt(args[1]);
+                    input = args[2];
+
+                    ParticleAnimation pa = new ParticleAnimation();
+                    String output = pa.animate(speed, input);
+
+                    System.out.println(output);
+                } else {
+                    System.out.println(errorMsgTerminal);
+                }
+
+                // Benchmarking - not enough time to implement into running with console
+
+                /*List<String> argsList = Arrays.asList(args);
+                if (argsList.contains("-runAsBenchmark")) {
+                    runBenchmark = true;
+                }
+
+                if (runBenchmark) {
+                    try {
+                        getMissingLettersBenchmark(input);
+                    } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException | InstantiationException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    MissingLetters missingLetters = new MissingLetters();
+                    missingLetters.getMissingLetters(input);
+                }*/
+            }
+        } catch(Exception e){
+            System.out.println(errorMsgTerminal);
             e.printStackTrace();
         }
 
